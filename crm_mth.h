ee119018dc7f337a020733e480c79d6e58fce772
Ryan Mangeno <ryanmangeno@gmail.com>
2024-12-26 23:28:44 -0500

    anonymous struct in vec4 for rgba

--- crm_mth.h
+++ crm_mth.h
@@ -89,6 +89,11 @@ namespace crm {
 			struct {
 				float x, y, z, w;
 			};
+
+			// for colors
+			struct {
+				float r, g, b, a;
+			};
 		};
 		vec4(float x = 0.0f, float y = 0.0f, float z = 0.0f, float w = 0.0f);
 	};
